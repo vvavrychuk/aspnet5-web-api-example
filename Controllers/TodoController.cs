ba0e5ece177ba646d1f1eb5de30d4102c1121004
Vasyl Vavrychuk <vvavrychuk@gmail.com>
2016-11-25 12:38:58 +0200

    example Web API

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace ExampleApi.Controllers
+{
+	public class TodoData
+	{
+		public string Item { get; set; }
+	}
+
+    public class TodoController : ApiController
+    {
+		// depency injection
+		static List<string> items = new List<string>();
+
+		public IHttpActionResult Get()
+        {
+			return Json(items);
+        }
+
+		public IHttpActionResult Post([FromBody] TodoData item)
+		{
+			items.Add(item.Item);
+			return Ok();
+		}
+    }
+}
